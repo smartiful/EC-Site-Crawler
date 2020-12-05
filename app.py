8f7a3d5ba1a3ccdd3c63a6c3c8e8407fc4bfe52e
a724412639 <a6504130@gmail.com>
2020-12-06 08:44:11 +0900

    添加日志，fake user agent，异步支持，修复爬取逻辑

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import os
+import re
+import time
+import trio
+import json
+import httpx
+import random
+import asyncio
 import requests
 import urllib.request
 from loguru import logger
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+
 
 BASE_URL = 'https://zozo.jp'
 ZOZOTOWN_CATEGORY_URL = BASE_URL + '/category/'
@@ -15,91 +23,100 @@ logger.add(
     level='DEBUG', encoding='utf-8'
 )
 
-driver = webdriver.Chrome()
-driver.get(BASE_URL)
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options,
+                          executable_path=r'H:\Python38\chromedriver.exe')
+
+with open('./data/fake_user_agent.txt', 'r') as f:
+    user_agent_list = json.load(f)
 
 
 class EcSiteCrawler():
     def __init__(self):
-        self.logger = logger
         self.all_category_url = self.get_all_category_url()
 
     # downloader
-    def downloader(self, iamge_url: str, category: str) -> int:
-        if 'p_gtype=2' in category:
-            file_path = f'./image_data{category.replace("?p_gtype=2", "used")}'
-        else:
-            file_path = f'./image_data{category}'
-        result = requests.get(iamge_url)
+    async def downloader(self, image_url: str, category: str, product_id: str) -> int:
+        category = re.sub(r"[,.!?:;'|\\【】]", "", category)
+        file_path = f'./image_data/{category}{product_id}'
+
         if not os.path.exists(file_path):
+            logger.info(f'created path {file_path}')
             os.makedirs(file_path)
-        with open(file_path + iamge_url[iamge_url.rindex("/"):], 'wb') as outfile:
-            outfile.write(result.content)
-        self.logger.success(f'url :{iamge_url} | category: {category}')
+        else:
+            logger.info(f'pass product {product_id}')
+            return
+
+        try:
+            async with httpx.AsyncClient() as client:
+                result = await client.get(image_url, headers={'User-Agent': random.choice(user_agent_list)})
+                with open(file_path + image_url[image_url.rindex("/"):], 'wb') as f:
+                    f.write(result.content)
+                    logger.success(f'url: {image_url} | category: {category} ')
+        except Exception as e:
+            logger.error(e)
 
     # decode and parse url
     def soup_url(self, url: str) -> BeautifulSoup:
         result = requests.get(url)
         result.encoding = result.apparent_encoding
         soup = BeautifulSoup(result.text, "html.parser")
+        logger.success(f'Requested url: {url}')
         return soup
 
     # get all category url
     def get_all_category_url(self) -> list:
         category_url_list = []
+
         zozotown_soup = self.soup_url(ZOZOTOWN_CATEGORY_URL)
         h2 = zozotown_soup.find_all('h2')
         for tag in h2:
             category_url = BASE_URL + tag.a.get('href')
             category_url_list.append(category_url)
+            logger.success(f'find category url: {category_url}')
 
         zozoused_soup = self.soup_url(ZOZOUSED_CATEGORY_URL)
         h2 = zozoused_soup.find_all('h2')
         for tag in h2:
             category_url = BASE_URL + tag.a.get('href')
             category_url_list.append(category_url)
-
-        with open('./data/all_category_url.txt', 'w') as f:
-            for url in category_url_list:
-                f.write(url)
-                f.write('\n')
+            logger.success(f'find category url: {category_url}')
 
         return category_url_list
 
     # get category product url
     def get_category_product_url(self) -> list:
         product_url_list = []
+
         count_product = len(driver.find_elements_by_xpath(
             '//*[@id="searchResultList"]/li/div/a')) + 1
 
         for product in range(1, count_product):
             product_url = driver.find_element_by_xpath(
                 f'//*[@id="searchResultList"]/li[{product}]/div/a').get_attribute('href')
-            # pass AD pages
-            if 'e.s4p.jp' not in product_url:
+            product_id = re.findall("\d+", product_url)[0]
+            # pass AD url and duplicate product
+            if 'e.s4p.jp' not in product_url and any(product_id in url for url in product_url_list) == False:
                 product_url_list.append(product_url)
+                logger.success(f'find product url: {product_url}')
         return product_url_list
 
     # get category name from product page
-    def get_category_name(self) -> str:
-        category_url = driver.find_element_by_xpath(
-            '//*[@id="breadCrumb"]/ul/li[2]/a').get_attribute('href')
-        category_name = category_url.replace('https://zozo.jp', '')
+    async def get_category_name(self) -> str:
+        category_name = driver.find_element_by_xpath(
+            '//*[@id="itemDetailInfo"]/div/dl/dd[2]/ul').text
+        category_name = category_name.replace(
+            '/', '&').replace('>\n', '/') + '/'
         return category_name
 
-    def dump_next_page(self):
-        try:
-            if driver.find_element_by_class_name('next'):
-                driver.find_element_by_class_name('next').click()
-        except NoSuchElementException:
-            return False
-        return True
-
     # download image from product page
-    def get_product_image(self, product_url: str) -> list:
+    async def get_product_image(self, product_url: str) -> list:
         image_list = []
         driver.get(product_url)
-        category_name = self.get_category_name()
         image_count = len(
             driver.find_element_by_id('photoThimb').find_elements_by_tag_name('li')) + 1
 
@@ -107,29 +124,41 @@ class EcSiteCrawler():
             image_url = driver.find_element_by_xpath(
                 f'//*[@id="photoThimb"]/li[{image}]/div/span/img').get_attribute('src')
             image_url = image_url.replace('35', '500')
-
             image_list.append(image_url)
         return image_list
 
-        #self.downloader(image_url, category_name)
+    # download product image
+    def download_product_image(self, product_url: str):
+        image_url_list = asyncio.run(self.get_product_image(product_url))
+        category_name = asyncio.run(self.get_category_name())
+        product_id = re.findall("\d+", product_url)[0]
+        for image_url in image_url_list:
+            trio.run(self.downloader, image_url, category_name, product_id)
 
     # start crawler
     def start_crawler(self):
-        # 1. get all category url
-        # 2. get product url list from category url
-        # 3. download image from product page
-        # 4. jump to next product page
-        # 5. jump to next category url
+        logger.info('start crawler')
         for category_url in self.all_category_url:
-            print(category_url)
             driver.get(category_url)
-            product_url_list = self.get_category_product_url()
-            for product_url in product_url_list:
-                image_url_list = self.get_product_image(product_url)
-                category_name = self.get_category_name()
-                for image_url in image_url_list:
-                    self.downloader(image_url, category_name)
-            if self.dump_next_page():
-                self.dump_next_page()
-            else:
-                break
+            index = 1
+            logger.info(f'start crawling category pages: {category_url}')
+
+            while True:
+                index_url = category_url + '?pno=' + str(index)
+                product_url_list = self.get_category_product_url()
+
+                for product_url in product_url_list:
+                    self.download_product_image(product_url)
+
+                driver.get(index_url)
+                try:
+                    driver.find_element_by_class_name('next').click()
+                    index += 1
+                    logger.info(f'dump to next page {index}')
+                except NoSuchElementException:
+                    logger.info(f'last page {index}')
+                    break
+
+
+start = EcSiteCrawler()
+start.start_crawler()
